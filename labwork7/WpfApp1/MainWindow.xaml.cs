c64c684fde89df1d08e6ab2daeac0b0184f57c56
pacipsyche <148044701+pacipsyche@users.noreply.github.com>
2024-10-24 18:09:21 +0700

    add labwork7

---
+++ labwork7/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class ButtonWindow : Window
+    {
+        private Random random = new Random();
+
+        public ButtonWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            double newLeft = random.Next(0, (int)(this.ActualWidth - Button.ActualWidth));
+            double newTop = random.Next(0, (int)(this.ActualHeight - Button.ActualHeight));
+
+            DoubleAnimation leftAnimation = new DoubleAnimation
+            {
+                To = newLeft,
+                Duration = TimeSpan.FromMilliseconds(300)
+            };
+
+            DoubleAnimation topAnimation = new DoubleAnimation
+            {
+                To = newTop,
+                Duration = TimeSpan.FromMilliseconds(300)
+            };
+
+            Button.BeginAnimation(Canvas.LeftProperty, leftAnimation);
+            Button.BeginAnimation(Canvas.TopProperty, topAnimation);
+        }
+    }
+}
---
+++ labwork7/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace WpfApp2
+{
+    public partial class PulsarWindow : Window
+    {
+        public PulsarWindow()
+        {
+            InitializeComponent();
+            AnimatePulsar();
+        }
+
+        private void AnimatePulsar()
+        {
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0,
+                To = 0.5,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            InnerStop.BeginAnimation(GradientStop.OffsetProperty, animation);
+        }
+    }
+}
